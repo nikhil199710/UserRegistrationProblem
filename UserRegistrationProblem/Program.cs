0f799910549a587d8e730d99d10a2bfd768d4330
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-15 19:17:05 +0530

    UC1ValidateFirstName

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter the user name for registration");
+                string input = Console.ReadLine();
+                if (input == "")
+                    break;
+                UserRegistration userRegistration = new UserRegistration();
+                bool validationForUserName = userRegistration.UserPatternMaking(input);
+                if (validationForUserName)
+                {
+                    Console.WriteLine($"The user name- {input} is valid");
+                }
+                else
+                {
+                    Console.WriteLine("The name does not match specified condition");
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class UserRegistration
+    {
+        public bool UserPatternMaking(string input)
+        {
+            string firstNamePattern = "[A-Z]{1}[A-Za-z]{2,}";
+            return Regex.IsMatch(input, firstNamePattern);
+
+        }
+    }
+}
